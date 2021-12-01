2a9044ddc17eec8ac74210dab968244294f9ea71
coolawsome2009 <92763757+coolawsome2009@users.noreply.github.com>
2021-11-30 21:03:02 -0500

    i accedently made da blizzard box 25 coins instead of 20

--- global/spamOpenBoxes.js
+++ global/spamOpenBoxes.js
@@ -10,7 +10,7 @@ const capitalize = (text) => text.charAt(0).toUpperCase() + text.slice(1);
             }
         }),
         price = ({
-            blizzard: 25,
+            blizzard: 20,
             spooky: 25,
             blizzard: 25,
             aquatic: 25,
