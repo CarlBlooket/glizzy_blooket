81cca9ad9a42ca95eca21bca825d8e6ceba3f366
glizzz_y <73669084+glixzzy@users.noreply.github.com>
2021-12-20 20:17:23 -0600

    remove comments so it can work for bookmarklets

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,16 +1,8 @@
-//  --> PLEASE READ THIS!!! <---
-//  
-//  Do not change the numbers in this file! Blooket has a hard limit on how many tokens
-//  you can add everyday. The limit is 500 and you cannot bypass it. Do not mess with
-//  with this file if you do not know what you are doing!!!
-//
-
-
 async function getName(authToken) {
     const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+' + authToken);
     const data = await response.json();
 
-    return data.name
+    return data.name;
 };
 
 async function addCurrencies() {
@@ -18,8 +10,8 @@ async function addCurrencies() {
     const myToken = localStorage.token.split('JWT ')[1];
 
     if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.')
-    }
+        alert('You can only add up to 500 tokens daily.');
+    };
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
         method: "PUT",
